86967f6dbc1e3deca5a6039a10a6de7540f11b0f
Miguel Guajardo <miguelguajardo1468@hotmail.com>
2022-05-24 12:51:15 -0300

    first commit

---
+++ paginas/script-cuestionario.js
@@ -0,0 +1,64 @@
+class Quiz {
+    constructor(question, choices, answer) {
+      this.question = question;
+      this.choices = choices;
+      this.answer = answer;
+    }
+  }
+  const containerQuiz = [
+    {
+      question:
+        "¿En cual de los siguientes países se pueden encontrar los gauchos?",
+      choices: ["Argelia", "Mexico", "Argentina", "Nueva Zelanda"],
+      answer: "Argentina",
+    },
+    {
+      question:
+        "¿En que ciudad estadounidense se basa el mapa del GTA: Vice City?",
+      choices: ["New york", "Miami", "Las vegas", "Los Angeles"],
+      answer: "Miami",
+    },
+    {
+      question: "¿Por donde absorben agua los arboles?",
+      choices: ["Hojas", "Raíces", "Tronco", "Ramas"],
+      answer: "Raíces",
+    },
+    {
+      question: "¿Que jugador gano el balón de oro en el año 2010?",
+      choices: [
+        "Lionel Messi",
+        "Cristiano Ronaldo",
+        "Yaya Toure",
+        "Pogba",
+      ],
+      answer:"Lionel Messi",
+    },
+    {
+      question: "¿Cuantos dedos tiene Bart Simpson?",
+      choices: ["3", "5", "6", "4"],
+      answer: "4",
+    },
+  ];
+  const questionText = document.getElementById("question");
+  const choice = document.getElementById("choices");
+  let text = document.createElement("h2");
+  let indexPregunta = 4;
+  text.innerHTML = containerQuiz[indexPregunta].question;
+  questionText.appendChild(text);
+for(i=0;i < 4; i++){
+  let optionChoice = document.createElement("button");
+  optionChoice.setAttribute("class","btn")
+  if(i === 0){
+    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[0];
+  }else if(i === 1) {
+    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[1];
+  }else if(i === 2) {
+    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[2];
+  }else{
+    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[3];
+  }
+  choice.appendChild(optionChoice);
+}
+const progress = document.getElementById("progress");
+let progresive = indexPregunta+1;
+progress.innerHTML = "Pregunta " + progresive + " de " + containerQuiz.length;
---
+++ script.js
@@ -0,0 +1,30 @@
+//Section 1
+let quizz = ["Q","U","I","Z","Z"];
+const UL = document.createElement("ul");
+UL.setAttribute("class", "ul");
+const sectionOne = document.getElementById("section-1");
+sectionOne.appendChild(UL);
+for(i = 0;i < quizz.length ;i++){
+    const LI = document.createElement("li");
+    LI.setAttribute("class", "li");
+    LI.innerHTML = quizz[i];
+    UL.appendChild(LI);
+}
+//Section 2
+const HTWO = document.createElement("h2");
+const sectionTwo = document.getElementById("section-2");
+sectionTwo.appendChild(HTWO);
+HTWO.setAttribute("class", "h2-tutorial");
+HTWO.innerHTML = "¿COMO JUGAR?";
+
+const tutorial = ["Presione el boton jugar que encontrara debajo para comenzar con el quiz","Lea atentamente el enunciado para responder","Seleccione la opcion que le parezca conveniente"]
+const nodoOL = document.createElement("ol");
+nodoOL.setAttribute("class","list");
+  for(let i=0;i<tutorial.length;i++)
+  {
+    const nodoLI = document.createElement("li");
+    nodoLI.setAttribute("class","list-item");
+    nodoLI.innerText=tutorial[i];
+    nodoOL.appendChild(nodoLI);
+  }
+sectionTwo.appendChild(nodoOL);
