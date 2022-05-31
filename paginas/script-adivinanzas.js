bec70bdb43fde98f40438dd8d0be45c2b8edf5fc
Miguel Guajardo <miguelguajardo1468@hotmail.com>
2022-05-31 14:14:49 -0300

    Agregado de Evento

--- paginas/script-adivinanzas.js
+++ paginas/script-adivinanzas.js
@@ -18,12 +18,66 @@ const containerAdivinados = [
     },
   ];
 
-  const questionText = document.getElementById("question");
-  const inputAnswer = document.getElementById("input");
-  let text = document.createElement("h2");
-  let indexPregunta = 0;
-  text.innerHTML = containerAdivinados[indexPregunta].question;
-  questionText.appendChild(text);
-  let input = document.createElement("input");
-  input.setAttribute("class","text");
-  inputAnswer.appendChild(input);
+  //-----------EVENTOS-------------
+  class Question{
+
+    QuestionIndex = 0;
+    score = 0;
+
+    constructor(questions){
+      this.questions = questions;
+    }
+    getQuestionIndex(){
+      return this.questions[this.QuestionIndex];
+    }
+    finalizarPregunta(){
+      return this.questions.length === this.QuestionIndex;
+    }
+    adivinar(answer){
+      if(this.getQuestionIndex().correctAnswer(answer)){
+        this.score++;
+      }
+      this.QuestionIndex++;
+    }
+  }
+  class NodoMostrar{
+    constructor(){}
+
+    mostrarPregunta(text){
+      const questionTitle = document.getElementById("question");
+    questionTitle.innerHTML = text;
+    }
+    mostrarInput(answer){
+      const inputContainer = document.getElementById("input");
+      const input = document.createElement("input");
+      input.setAttribute("id","text")
+      inputContainer.appendChild(input);
+      input.addEventListener("input", compararRespuesta);
+    }
+  }
+  const newQuestions = containerAdivinados.map(question => new Adivinazas(question.question,question.answer));
+
+  function siguientePregunta(pregunta,mostrar1){
+    if(pregunta.finalizarPregunta()){
+      console.log(pregunta.score);
+      mostrar1.mostrarScore(pregunta.score);
+    }else{
+      mostrar1.mostrarPregunta(pregunta.getQuestionIndex().question);
+      mostrar1.mostrarInput();
+    }
+  }
+  function compararRespuesta(valor){
+    if(valor !== containerAdivinados.answer){
+      console.log("Incorrecto");
+    }else{
+      console.log("correcto");
+      
+    }
+  }
+  function iniciar(){
+    const pregunta = new Question(newQuestions);
+    const mostrar1 = new NodoMostrar();
+    
+    siguientePregunta(pregunta,mostrar1);
+   }
+   iniciar(); 
--- paginas/script-cuestionario.js
+++ paginas/script-cuestionario.js
@@ -4,6 +4,9 @@ class Quiz {
       this.choices = choices;
       this.answer = answer;
     }
+    correctAnswer(choice){
+      return choice ===this.answer;
+    }
   }
   const containerQuiz = [
     {
@@ -11,12 +14,6 @@ class Quiz {
         "¿En cual de los siguientes países se pueden encontrar los gauchos?",
       choices: ["Argelia", "Mexico", "Argentina", "Nueva Zelanda"],
       answer: "Argentina",
-    },
-    {
-      question:
-        "¿En que ciudad estadounidense se basa el mapa del GTA: Vice City?",
-      choices: ["New york", "Miami", "Las vegas", "Los Angeles"],
-      answer: "Miami",
     },
     {
       question: "¿Por donde absorben agua los arboles?",
@@ -38,95 +35,158 @@ class Quiz {
       choices: ["3", "5", "6", "4"],
       answer: "4",
     },
+    {
+      question: "Que empresa fundo bill gates?",
+      choices: ["Apple",
+      "IBM",  
+      "Amazon",
+      "Microsoft",
+      ],
+      answer:"Microsoft",
+    },
+    {
+      question: "Cual de los siguiente poetas Españoles murio fusilado cerca de Granada durante la guerra civil Española?",
+      choices: ["Miguel Hernández",
+      "Manuel Gómez del Valle",  
+      "Federico Gárcia Lorca",
+      "Antonio Machado",
+      ],
+      answer:"Federico Gárcia Lorca",
+    },
+    {
+      question: "En qué año nacio el escultor flamenco Juan de Bolonia?",
+      choices: ["1428",
+      "1673",  
+      "1501",
+      "1529",
+      ],
+      answer:"1529",
+    },
+    {
+      question: "Bernado o'Higgins se convirtió en gobernante de qué país en 1817",
+      choices: ["Brasil",
+      "Irlanda",  
+      "Chile",
+      "Perú",
+      ],
+      answer:"Chile",
+    },
+    {
+      question: "Porqué se les pone una funda de tela o espuma a los micrófonos?",
+      choices: ["Para proteger contra el frio",
+      "Para proteger contra interferencias de sonido",  
+      "Para cumplir con reglas de higiene",
+      "Para satisfacer las tendencias de moda",
+      ],
+      answer:"Para proteger contra interferencias de sonido",
+    },
+    {
+      question: "Hacia qué tiene atracción una persona con pogonofilia?",
+      choices: ["Barba",
+      "Ojos claros",  
+      "Moda elegante",
+      "Cabello largo",
+      ],
+      answer:"Barba",
+    },
+    {
+      question: "En la vida de qué prócer argentino está basada la película llamada "+"El santo de la espada"+ "",
+      choices: ["Manuel Belgrano",
+      "José de San Martín",  
+      "Martín de Güemes",
+      "Juan Manuel de Rosas",
+      ],
+      answer:"José de San Martín",
+    },
+    {
+      question: "Qué es la enfermedad del Parkinson",
+      choices: ["Una enfermedad de la vista",
+      "Enfermedad crónica y degenerativa del sistema nervioso",  
+      "Una enfermedad progresiva",
+      "Una enfermedad en la piel",
+      ],
+      answer:"Enfermedad crónica y degenerativa del sistema nervioso",
+    },
+    {
+      question: "Qué es la dislexia",
+      choices: ["Habilidad para resolver acertijos",
+      "Capacidad para ejecutar órdenes",  
+      "Trastorno del aprendizaje de la lecto escritura",
+      "Desarrollo simple del lóbulo temporal del cerebro",
+      ],
+      answer:"Trastorno del aprendizaje de la lecto escritura",
+    },
   ];
-  const questionText = document.getElementById("question");
-  const choice = document.getElementById("choices");
-  let text = document.createElement("h2");
-  let indexPregunta = 0;
-  let score = 0;
-  text.innerHTML = containerQuiz[indexPregunta].question;
-  questionText.appendChild(text);
-for(i=0;i < 4; i++){
-  let optionChoice = document.createElement("button");
-  optionChoice.setAttribute("id","btn-"+i)
-  if(i === 0){
-    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[0];
-  }else if(i === 1) {
-    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[1];
-  }else if(i === 2) {
-    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[2];
-  }else{
-    optionChoice.innerHTML = containerQuiz[indexPregunta].choices[3];
-  }
-  choice.appendChild(optionChoice);
-}
-const progress = document.getElementById("progress");
-let progresive = indexPregunta+1;
-progress.innerHTML = "Pregunta " + progresive + " de " + containerQuiz.length;
-/*
-//------------EVENTOS-----------------
-const boton0 = document.getElementById("btn-0");
-boton0.addEventListener("click", ()=>{
-  if(boton0.innerText === containerQuiz[indexPregunta].answer){
-    boton0.setAttribute("id","correcto");
-    boton1.setAttribute("id","disable")
-    boton2.setAttribute("id","disable")
-    boton3.setAttribute("id","disable")
-    score +=1;
-  }else{
-    boton0.setAttribute("id","incorrecto");
-    boton1.setAttribute("id","disable")
-    boton2.setAttribute("id","disable")
-    boton3.setAttribute("id","disable")
+  //-----------EVENTOS-------------
+  class Question{
 
-  }
-});
+    QuestionIndex = 0;
+    score = 0;
 
-const boton1 = document.getElementById("btn-1");
-boton1.addEventListener("click", ()=>{
-  if(boton1.innerText === containerQuiz[indexPregunta].answer){
-    boton1.setAttribute("id","correcto");
-    boton0.setAttribute("id","disable")
-    boton2.setAttribute("id","disable")
-    boton3.setAttribute("id","disable")
-    score +=1;
-  }else{
-    boton1.setAttribute("id","incorrecto");
-    boton0.setAttribute("id","disable")
-    boton2.setAttribute("id","disable")
-    boton3.setAttribute("id","disable")
+    constructor(questions){
+      this.questions = questions;
+    }
+    getQuestionIndex(){
+      return this.questions[this.QuestionIndex];
+    }
+    finalizarPregunta(){
+      return this.questions.length === this.QuestionIndex;
+    }
+    adivinar(answer){
+      if(this.getQuestionIndex().correctAnswer(answer)){
+        this.score++;
+      }
+      this.QuestionIndex++;
+    }
   }
-});
+  class NodoMostrar{
+    constructor(){}
 
-const boton2 = document.getElementById("btn-2");
-boton2.addEventListener("click", ()=>{
-  if(boton2.innerText === containerQuiz[indexPregunta].answer){
-    boton2.setAttribute("id","correcto");
-    boton0.setAttribute("id","disable")
-    boton1.setAttribute("id","disable")
-    boton3.setAttribute("id","disable")
-    score +=1;
-  }else{
-    boton2.setAttribute("id","incorrecto");
-    boton0.setAttribute("id","disable")
-    boton1.setAttribute("id","disable")
-    boton3.setAttribute("id","disable")
+    mostrarPregunta(text){
+      const questionTitle = document.getElementById("question");
+    questionTitle.innerHTML = text;
+    }
+    mostrarOpciones(choices, llamar) {
+      const choicesOfQuestion = document.getElementById("choices");
+      choicesOfQuestion.innerHTML = "";
+      for(let i = 0; i < 4 ;i++){
+        const button = document.createElement("button");
+        button.setAttribute("id", "btn-"+i);
+        button.innerText = choices[i];
+        button.addEventListener("click", ()=> llamar(choices[i]));
+        choicesOfQuestion.appendChild(button);
+    };
   }
-});
-
-const boton3 = document.getElementById("btn-3");
-boton3.addEventListener("click", ()=>{
-  if(boton3.innerText === containerQuiz[indexPregunta].answer){
-    boton3.setAttribute("id","correcto");
-    boton0.setAttribute("id","disable")
-    boton1.setAttribute("id","disable")
-    boton2.setAttribute("id","disable")
-    score +=1;
-  }else{
-    boton3.setAttribute("id","incorrecto");
-    boton0.setAttribute("id","disable")
-    boton1.setAttribute("id","disable")
-    boton2.setAttribute("id","disable")
+  mostrarScore(score){
+    const puntaje = `
+      <h1>Puntaje</h1>
+      <h2>Tu puntaje fue de ${score}</h2>
+      
+    `;
+    const contenedorPuntaje = document.getElementById("quiz");
+    contenedorPuntaje.innerHTML = puntaje;
     
   }
-});*/
+}
+  const newQuestions = containerQuiz.map(question => new Quiz(question.question,question.choices,question.answer,question.info));
+
+  function siguientePregunta(pregunta,mostrar1){
+    if(pregunta.finalizarPregunta()){
+      console.log(pregunta.score);
+      mostrar1.mostrarScore(pregunta.score);
+    }else{
+      mostrar1.mostrarPregunta(pregunta.getQuestionIndex().question);
+      mostrar1.mostrarOpciones(pregunta.getQuestionIndex().choices, (parametroChoice)=> {
+        pregunta.adivinar(parametroChoice);
+        siguientePregunta(pregunta,mostrar1);
+      });
+    }
+  }
+
+  function iniciar(){
+   const pregunta = new Question(newQuestions);
+   const mostrar1 = new NodoMostrar();
+   
+   siguientePregunta(pregunta,mostrar1);
+  }
+  iniciar(); 
